c22f484dab284b45f44e15cc83d551c86a7ac946
Virtual Luxury <ryuuaka.mail@gmail.com>
2020-08-27 00:28:49 -0500

    nueva versión

--- mp3cd.py
+++ mp3cd.py
@@ -1,63 +1,96 @@
 # -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
 import urllib2
+import webbrowser
 import sys
+import argparse
 import webbrowser
 
-# By Virtual Luxury
+# apt install python-bs4
 
-url = "http://mp3clan.top/mp3/" #actualizar en caso de que mp3clan cambie de dominio
-song_urls = []
-song_names = []
+# update if necessary
+MP3CLAN_URL = 'http://mp3clan.top/mp3/'
+TEMPLATE_FILE = 'resources/template.txt'
+FILENAME = 'resultados.html'
 
-def search(songname):
-	print "\n   ███╗   ███╗██████╗ ██████╗  ██████╗██████╗ "
-	print "   ████╗ ████║██╔══██╗╚════██╗██╔════╝██╔══██╗"
-	print "   ██╔████╔██║██████╔╝ █████╔╝██║     ██║  ██║"
-	print "   ██║╚██╔╝██║██╔═══╝  ╚═══██╗██║     ██║  ██║"
-	print "   ██║ ╚═╝ ██║██║     ██████╔╝╚██████╗██████╔╝"
-	print "   ╚═╝     ╚═╝╚═╝     ╚═════╝  ╚═════╝╚═════╝ \n"
-	print "Buscando..."
-	songnameclean = songname.replace(" ","_")
-	complete = False
-	while complete==False:
-		try:
-			respuesta = urllib2.urlopen(url+songnameclean)
-			linea = respuesta.readline()
-			while linea!="":
-				if linea.find('<li class="mp3list-play" id=')>=0:
-					split_1 = linea.split("data-title=")
-					temp = split_1[1].replace(">","")
-					temp = temp.replace('"','')
-					song_names.append(temp.strip())
-					linea = respuesta.readline()
-					linea = respuesta.readline()
-					split_1 = linea.split('rel=')
-					song_urls.append(split_1[0].strip()) 
-				linea = respuesta.readline()
-			finalhtml = '<html><head><meta charset="utf-8"><title>Resultados de: '+songname+'</title><link rel="stylesheet" type="text/css" href="resources/estilos.css" media="screen"></head><body><article><h1>Resultados de la búsqueda:</h1>'
-			position = 0
-			for element in song_names:
-				finalhtml += "<p>" + str(position+1) + ". " + str(song_urls[position]) + ">" + str(song_names[position]) + "</a></p>"
-				position += 1
-			finalhtml += '</article></body><html>'
-			file = open("resultados.html","w")
-			file.write(finalhtml)
-			file.close()
-			if len(song_names)>0:
-				print(str(len(song_names))+ " melodías encontradas. Abriendo navegador.")
-				webbrowser.open('resultados.html')
-			else:
-				print(str(len(song_names))+ " melodías encontradas.")
-			complete = True
-		except Exception:
-			print "Error en la conexión."
-			complete = True
+class MusicFile:
+	song_name = ''
+	download_url = ''
 
+def generate_results(music_list):
+	try:
+		final_file = ''
+		with open(TEMPLATE_FILE) as template:
+			final_file = template.read()
+		for element in music_list:
+			final_file += '<p>' + str(element.song_name) + '</p>'
+			final_file += '<audio controls><source src="' + str(element.download_url) + '"></audio>'
+		final_file += '</article></body><html>'
+		file = open(FILENAME, 'w')
+		file.write(final_file)
+		file.close()
+		webbrowser.open(FILENAME)
+	except Exception, e:
+		print('Error generating ' + str(FILENAME))
+		print e
+
+def check_if_avalible(music_file):
+	try:
+		urllib2.urlopen(music_file.download_url)
+		return True
+	except urllib2.HTTPError, e:
+		print(e.code)
+	except urllib2.URLError, e:
+		print(e.args)
+	return False
+
+def search(song_name):
+	song_name = song_name.replace(' ', '_')
+	music_list = []
+	try:
+		web_page = urllib2.urlopen(MP3CLAN_URL + song_name)
+		soup = BeautifulSoup(web_page.read().decode("utf-8"), "html.parser")
+		mp3clan_list = soup.find_all("li", {"class": "mp3list-play"})
+		# remove first element because it's not a song
+		mp3clan_list.pop(0)
+		for element in mp3clan_list:
+			soup = BeautifulSoup(str(element), "lxml")
+			music_file = MusicFile()
+			music_file.song_name = (soup.find("div")).attrs.get('title')
+			music_file.download_url = (soup.find("a")).attrs.get('href')
+			music_list.append(music_file)
+	except Exception, e:
+		print('Problem searching music')
+		print e
+	return music_list
+
+def start_process(args):
+	print('Searching...')
+	music_list = search(str(args.s))
+	if args.c:
+		print('Validating...')
+		music_list_checked = []
+		for element in music_list:
+			if (check_if_avalible(element)):
+				music_list_checked.append(element)
+		music_list = music_list_checked
+	print('Generating html...')
+	generate_results(music_list)
 
 def main():
-	try:
-		search(sys.argv[1])
-	except Exception:
-		print "Faltan argumentos"
+	parser = argparse.ArgumentParser(description='Python mp3clan simple downloader')
+	required_arguments = parser.add_argument_group('required arguments')
+	required_arguments.add_argument('-s', '--search',
+						action='store',
+						required=True,
+						dest='s',
+						help='Search a song name')
+	parser.add_argument('-c', '--check', action='store_true',
+						dest='c',
+						help='Enable validate file URL step')
+	parser.add_argument('--version', action='version',
+						version='%(prog)s 1.0')
+
+	start_process(parser.parse_args())
 
 main()
